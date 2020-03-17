f3d34f530bf20773b130aa77dc820f1585def2fe
pzalejski <piotrzalejski@live.com>
2020-03-17 02:17:31 -0500

    added additional navigation and a create user/register-pz

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def index():
+def index():  # fucntions that are called in the url_for('')
     return render_template("home.html")
 
 
@@ -16,6 +16,36 @@ def blackwidow():
 @app.route("/dr-strange")
 def dr_strange():
     return render_template('dr_strange.html')
+
+
+@app.route("/ironman")
+def ironman():
+    return render_template('ironman.html')
+
+
+@app.route("/thor")
+def thor():
+    return render_template('thor.html')
+
+
+@app.route("/hawkeye")
+def hawkeye():
+    return render_template('hawkeye.html')
+
+
+@app.route('/captain-america')
+def captain_america():
+    return render_template('captain_america.html')
+
+
+@app.route('/hulk')
+def hulk():
+    return render_template('hulk.html')
+
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
 
 
 if __name__ == "__main__":
